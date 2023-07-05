76e6c813c4b0447e41b97dd9e8be0d2e77f11a14
javier-mansillal <javier.mansillal@usm.cl>
2023-07-05 18:16:24 -0400

    Se han hecho cambios en API, modificado algunas cosas del core y algunos settings.

--- tiger_car_boutique/api/views.py
+++ tiger_car_boutique/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import VehiculoSerializer
+from core.models import Vehiculo
 # Create your views here.
+
+class VehiculoViewSet(viewsets.ModelViewSet):
+    queryset = Vehiculo.objects.filter(es_nuevo=True).values()
+    serializer_class = VehiculoSerializer
--- tiger_car_boutique/core/models.py
+++ tiger_car_boutique/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Vehiculo(models.Model):
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=200)
+    año = models.IntegerField()
+    kilometraje = models.IntegerField()
+    precio = models.IntegerField()
+    vehiculo = models.CharField(max_length=300)
+    es_nuevo = models.BooleanField()
+    
+    def __str__(self):
+        return self.vehiculo
